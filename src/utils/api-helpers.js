8cb363cf24a6fec71f93eb4209b8415fd6f21529
Petr <p.y.klimenko@gmail.com>
2024-09-20 10:17:16 +0300

    upd

--- src/utils/api-helpers.js
+++ src/utils/api-helpers.js
@@ -1,15 +1,19 @@
 export async function handleApiRequest(url, body, method = 'POST') {
-    try {
-      const response = await fetch(url, {
-        method,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body),
-      });
-      if (!response.ok) throw new Error('Ошибка запроса');
-      return await response.json();
-    } catch (error) {
-      console.error('Ошибка при выполнении запроса', error);
-      return null;
+  try {
+    const options = {
+      method,
+      headers: { 'Content-Type': 'application/json' }
+    };
+    
+    if (method !== 'GET') {
+      options.body = JSON.stringify(body);
     }
+
+    const response = await fetch(url, options);
+    if (!response.ok) throw new Error('Ошибка запроса');
+    return await response.json();
+  } catch (error) {
+    console.error('Ошибка при выполнении запроса', error);
+    return null;
   }
-  
+}
