34eb667994b5777b80e0eea4a2e491b94b789168
Petr <p.y.klimenko@gmail.com>
2024-09-20 18:06:05 +0300

    upd 8

--- src/components/teacher/teacherStatistics.js
+++ src/components/teacher/teacherStatistics.js
@@ -1,6 +1,6 @@
 // src/components/teacher/teacherStatistics.js
 import React, { useEffect, useState } from 'react';
-import { handleApiRequest } from '../utils/api-helpers';
+import { handleApiRequest } from '../../utils/api-helpers';
 import logger from '../utils/logger';
 
 function TeacherStatistics() {
