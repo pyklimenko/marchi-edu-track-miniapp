ea0cdfd4d09924a1b2ed6da020e3c75fe4f8c523
Petr <p.y.klimenko@gmail.com>
2024-09-20 16:41:16 +0300

    upd ошибка в названиях

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './context/UserContext'; // Импортируем UserProvider
+import { UserProvider } from './context/userContext'; // Импортируем UserProvider
 import './styles.css';
 import App from './App';
 
