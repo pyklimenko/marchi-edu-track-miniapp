714576d6cfd3e1479e1a5ee23bf9989b629bd79b
Пётр Клименко <p.y.klimenko@gmail.com>
2024-09-22 16:18:51 +0300

    Убрал часть функций из-за ограничений vercel

--- src/components/teacher/teacherDashboard.js
+++ src/components/teacher/teacherDashboard.js
@@ -2,12 +2,10 @@
 import React, { useState } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import TeacherProfile from './teacherProfile';
-import TeacherStatistics from './teacherStatistics';
 import TeacherQRGenerator from './teacherQRGenerator';
 import TeacherAttendance from './teacherAttendance';
 import { Container, Tabs, Tab } from '@mui/material';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import InsertChartOutlinedIcon from '@mui/icons-material/InsertChartOutlined';
 import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
 import ListIcon from '@mui/icons-material/List';
 
@@ -20,10 +18,8 @@ function TeacherDashboard() {
 
     // Перенаправляем на нужный маршрут при изменении вкладки
     if (newValue === 0) navigate('profile');
-    if (newValue === 1) navigate('attendance');
-    if (newValue === 2) navigate('statistics');
-    if (newValue === 3) navigate('qr');
-
+    if (newValue === 1) navigate('qr');
+    if (newValue === 2) navigate('attendance');
   };
 
   return (
@@ -31,15 +27,13 @@ function TeacherDashboard() {
       {/* Вкладки с иконками */}
       <Tabs value={value} onChange={handleChange} aria-label="icon tabs" centered>
         <Tab icon={<AccountCircleOutlinedIcon />} aria-label="profile" label="Profile" />
-        <Tab icon={<ListIcon />} aria-label="attendance" label="Attendance" />
-        <Tab icon={<InsertChartOutlinedIcon />} aria-label="statistics" label="Statistics" />
         <Tab icon={<CameraAltOutlinedIcon />} aria-label="qr" label="QR" />
+        <Tab icon={<ListIcon />} aria-label="attendance" label="Attendance" />
       </Tabs>
 
       {/* Маршруты */}
       <Routes>
         <Route path="profile" element={<TeacherProfile />} />
-        <Route path="statistics" element={<TeacherStatistics />} />
         <Route path="qr" element={<TeacherQRGenerator />} />
         <Route path="attendance" element={<TeacherAttendance />} />
       </Routes>
